ba17dc70e2adeb1dd09bd3b4c40e6aa1b3a2bc51
Jose A. Fernandez <fernandezja@gmail.com>
2016-04-01 14:36:35 -0300

    App unificadora en NodeJS para ir agregando practicos del lado del servidor

---
+++ src/simple-nodejs-azure/jose/app.js
@@ -0,0 +1,15 @@
+﻿var app = function () {
+    var self = {};
+    
+    self.nombre = function () {
+        return "Jose A. Fernandez";
+    }
+
+    self.items = function () {
+        return "Estoy desde mi App";
+    }
+    
+    return self;
+}();
+
+module.exports = app;
---
+++ src/simple-nodejs-azure/server.js
@@ -0,0 +1,35 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+
+
+http.createServer(function (req, res) {
+    
+    //Verificar request
+    var posicionBarra = req.url.toString().indexOf('/', 1);
+    var carpeta = req.url.toString().substring(1, posicionBarra);
+
+    console.log(carpeta);
+    
+    if (carpeta.length > 3) {
+        var app = require('./' + carpeta + '/app.js');
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        res.write('<h1>');
+        res.write('Carpeta: ' + carpeta);
+        res.write('<br />');
+        res.write('Nombre: ' + app.nombre());
+        res.write('</h1>');
+        res.end(app.items());
+        
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.write('<h1>');
+        res.write('Carpeta no encontrada!');
+        res.end('</h1>');
+    }
+    
+    
+    
+}).listen(port);
+
