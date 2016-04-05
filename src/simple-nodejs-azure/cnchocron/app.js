e1df195ed08a34d7de9b43e7db1afe7566603041
cnchocron <cnchocron@gmail.com>
2016-04-05 13:41:01 -0300

    Creación de lista con 50 items

---
+++ src/simple-nodejs-azure/cnchocron/app.js
@@ -0,0 +1,23 @@
+﻿var app = function () {
+    var self = {};
+    
+    self.nombre = function () {
+        return "Cristhian Chocron";
+    }
+
+    self.items = function () {
+        
+        var miLista = '<ul>';
+
+        for (i = 1; i < 51; i++) {            
+            miLista += '<li> Item ' + i + '</li>';
+        }
+        
+        miLista += '</ul>';
+        return miLista;
+    }
+    
+    return self;
+}();
+
+module.exports = app;
