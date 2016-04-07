8800fa41f5883150796b79837c41efcff56ec592
MLEnzo <programasdeenzo@gmail.com>
2016-04-07 18:43:00 -0300

    archivo faltante app

---
+++ src/simple-nodejs-azure/enzo/app.js
@@ -0,0 +1,22 @@
+﻿var app = function () {
+    var self = {};
+    
+    self.nombre = function () {
+        return "Enzo Medina";
+    }
+    
+    self.items = function (cantidad) {
+        var unaLista='<ul>';
+        for (var i = 1; i <= cantidad; i++) {
+            unaLista = unaLista + '<li>Item'+i+' </li>';
+        };   
+        unaLista += '</ul>';
+        return unaLista;
+    }
+    
+
+    
+    return self;
+}();
+
+module.exports = app;
