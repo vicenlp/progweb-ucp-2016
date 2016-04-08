7b09a0a347e802d7dbc41466253bec5f61fd350c
MLEnzo <programasdeenzo@gmail.com>
2016-04-08 11:12:07 -0300

    se agrega form

--- src/simple-nodejs-azure/enzo/app.js
+++ src/simple-nodejs-azure/enzo/app.js
@@ -6,11 +6,14 @@
     }
     
     self.items = function (cantidad) {
-        var unaLista='<ul>';
+        var unaLista = '<ul>';
+        var unForm = '<form action="/enzo/demo.html" method="get">';
         for (var i = 1; i <= cantidad; i++) {
-            unaLista = unaLista + '<li>Item'+i+' </li>';
-        };   
+            unaLista = unaLista + '<li>Item' + i + ' </li>' + unForm + '<input type="submit" name="cantidad" value="' + i + '">';//+ ' <input type="header" name="" value="">';
+        };
+        unForm += '</form>';        
         unaLista += '</ul>';
+        
         return unaLista;
     }
     
