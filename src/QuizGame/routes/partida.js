8a0e903f1ac5521c52873f0977cc0a52b247979f
Jose A. Fernandez <fernandezja@gmail.com>
2016-04-21 16:26:50 -0300

    Nuevo proyecto QuizGame

---
+++ src/QuizGame/routes/partida.js
@@ -0,0 +1,25 @@
+﻿var express = require('express');
+var router = express.Router();
+
+
+router.get('/listado', function (req, res) {
+    
+    var partidaVm = { };
+
+    partidaVm.partidas = [
+        { descripcion: 'Partida 1' },
+        { descripcion: 'Partida 2' }
+    ];    
+
+    res.render('partidaListado', partidaVm);
+});
+
+router.get('/crear', function (req, res) {
+    
+    var partidaVm = {};
+    
+    
+    res.render('partidaCrear', partidaVm);
+});
+
+module.exports = router;
---
+++ src/QuizGame/routes/pregunta.js
@@ -0,0 +1,36 @@
+﻿var express = require('express');
+var router = express.Router();
+
+
+router.get('/:id', function (req, res) {
+    
+    var preguntadId = req.params.id;
+    console.log('preguntadId:', preguntadId);
+    
+    var pregunta = {
+        id : preguntadId,
+        descripcion: '¿Cual es el color favorito?',
+        respuestas: [
+            {
+                id: 1,
+                descripcion: 'Rta 1',
+                esCorrecta: false,
+            },
+            {
+                id: 2,
+                descripcion: 'Rta 2',
+                esCorrecta: true,
+            },
+            {
+                id: 3,
+                descripcion: 'Rta 3',
+                esCorrecta: false,
+            }
+        ]
+     }
+    
+
+    res.render('pregunta', pregunta);
+});
+
+module.exports = router;
