7f4c3a9ff9880a2a22d8426103bef6cb23981992
Jose A. Fernandez <fernandezja@gmail.com>
2016-04-21 18:57:09 -0300

    Logo beta de QuizGame

--- src/QuizGame/routes/index.js
+++ src/QuizGame/routes/index.js
@@ -1,9 +1,13 @@
 ﻿var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('home', { title: 'QuizGame'});
+});
+
+router.get('/main', function (req, res) {
+    res.render('index', { title: '' });
 });
 
 module.exports = router;
